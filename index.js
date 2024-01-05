f2755fe8a49be45e93b9b29aa2c3d18258ccc668
Dāvis Zālītis <k0d3r1s@gmail.com>
2024-01-06 01:08:19 +0200

    feat: remove jquery

--- index.js
+++ index.js
@@ -1,75 +1,67 @@
-let timeout = (new URLSearchParams(location.search).get("timeout") || 60) * 1000;
+const timeout = (new URLSearchParams(window.location.search).get("timeout") || 60) * 1000;
 
 let tick = 1;
 
-$(document).ready(randomImage, timeout);
-
-function randomImage() {
-	getImage();
-
-	setInterval(function() {
-		getImage();
-	}, timeout);
-}
+document.addEventListener("DOMContentLoaded", function () {
+    getImage();
+    setInterval(getImage, timeout);
+});
 
 function getImage() {
-	let width = viewportWidth();
-	let height = viewportHeight();
+    const width = viewportWidth();
+    const height = viewportHeight();
 
-	document.querySelector('meta[name=viewport]').setAttribute("content", "width=" + width + ",height=" + height + ",initial-scale=1");
+    document.querySelector('meta[name=viewport]').setAttribute("content", "width=" + width + ",height=" + height + ",initial-scale=1");
 
-	let seed = (Math.random() + 1).toString(36).substring(5);
-	let image = "https://picsum.photos/seed/" + seed + "/" + width + "/" + height + ".webp?" + tick;
+    const seed = (Math.random() + 1).toString(36).substring(5);
+    const image1x = "https://picsum.photos/seed/" + seed + "/" + width + "/" + height + ".webp?" + tick;
 
-	$("#img").attr("src", image);
+    let imgElement = document.getElementById("img");
+    imgElement.src = image1x;
 
-    let image2 = "https://picsum.photos/seed/" + seed + "/" + (width * 2) + "/" + (height * 2) + ".webp?" + tick;
-	$("#img").attr("srcset", image + " 1x, " + image2 + " 2x");
+    const image2x = "https://picsum.photos/seed/" + seed + "/" + (width * 2) + "/" + (height * 2) + ".webp?" + tick;
+    imgElement.srcset = image1x + " 1x, " + image2x + " 2x";
 
-	$("#img").attr("width", width);
-	$("#img").attr("height", height);
+    imgElement.width = width;
+    imgElement.height = height;
 
-	tick += 1;
+    tick += 1;
 }
 
 function viewportWidth() {
-	if (window.innerWidth) return window.innerWidth;
-	var doc = document,
-		html = doc && doc.documentElement,
-		body = doc && (doc.body || doc.getElementsByTagName("body")[0]),
-		getWidth = function(elm) {
-			if (!elm) return 0;
-			var setOverflow = function(style, value) {
-					var oldValue = style.overflow;
-					style.overflow = value;
-					return oldValue || "";
-				},
-				style = elm.style,
-				oldValue = setOverflow(style, "hidden"),
-				width = elm.clientWidth || 0;
-			setOverflow(style, oldValue);
-			return width;
-		};
-	return Math.max(getWidth(html), getWidth(body));
+    if (window.innerWidth) return window.innerWidth;
+
+    let doc = document, html = doc && doc.documentElement,
+        body = doc && (doc.body || doc.getElementsByTagName("body")[0]), getWidth = function (elm) {
+            if (!elm) return 0;
+            let setOverflow = function (style, value) {
+                let oldValue = style.overflow;
+                style.overflow = value;
+                return oldValue || "";
+            }, style = elm.style, oldValue = setOverflow(style, "hidden"), width = elm.clientWidth || 0;
+            setOverflow(style, oldValue);
+
+            return width;
+        };
+
+    return Math.max(getWidth(html), getWidth(body));
 }
 
 function viewportHeight() {
-	if (window.innerHeight) return window.innerHeight;
-	var doc = document,
-		html = doc && doc.documentElement,
-		body = doc && (doc.body || doc.getElementsByTagName("body")[0]),
-		getHeight = function(elm) {
-			if (!elm) return 0;
-			var setOverflow = function(style, value) {
-					var oldValue = style.overflow;
-					style.overflow = value;
-					return oldValue || "";
-				},
-				style = elm.style,
-				oldValue = setOverflow(style, "hidden"),
-				height = elm.clientHeight || 0;
-			setOverflow(style, oldValue);
-			return height;
-		};
-	return Math.max(getHeight(html), getHeight(body));
+    if (window.innerHeight) return window.innerHeight;
+
+    let doc = document, html = doc && doc.documentElement,
+        body = doc && (doc.body || doc.getElementsByTagName("body")[0]), getHeight = function (elm) {
+            if (!elm) return 0;
+            let setOverflow = function (style, value) {
+                let oldValue = style.overflow;
+                style.overflow = value;
+                return oldValue || "";
+            }, style = elm.style, oldValue = setOverflow(style, "hidden"), height = elm.clientHeight || 0;
+            setOverflow(style, oldValue);
+
+            return height;
+        };
+
+    return Math.max(getHeight(html), getHeight(body));
 }
